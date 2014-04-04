2ad2dbec65be582b7a7d9f385d226a2e8c835d7c
fmulp <val_tut@tut.by>
2014-04-04 13:31:24 +0300

    Create InternalSensors and OnboardPC modules

---
+++ InternalSensors/InternalSensors.ino
@@ -0,0 +1,16 @@
+// BrSTU Robotics lab, 2014
+
+
+// Connect 4 ultrasound sensors
+
+
+void setup()
+{
+	Serial.begin(9600);
+    
+}
+
+void Loop()
+{
+	
+}
---
+++ OnboardPC/Controller.py
@@ -0,0 +1,11 @@
+''' BrSTU Robotics Lab, 2014
+
+Controller Software for SubSurfaceMapper
+
+MotorDriver
+CameraDriver
+
+InternalSensors Module Protocol
+CoilDetector Protocol
+
+'''
